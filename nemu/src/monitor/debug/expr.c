5854c6d77939d3df1877465e9636f2505daf7232
3024244169-Wang Haoyu <3160303297@qq.com>
2025-09-14 13:05:44 +0800

    注释部分代码

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -82,12 +82,30 @@ static bool make_token(char *e) {
 	while(e[position] != '\0') {  //while执行的条件是e[position]不为字符串结束符，其中e是传入的字符串，position是字符串的索引
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
+			//对NR_REGEX的定义是 规则的数目(规则数组的大小/单个规则的大小)
+			//也就是说，这里对每个位置的token，都要尝试所有的规则
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+				//逐步分析if的成立条件：
+				//regexec 函数用于在字符串中查找是否有符合指定正则表达式的子串。
+				//int regexec(const regex_t *preg, const char *string, size_t nmatch, regmatch_t pmatch[], int eflags);
+				//汉语表述大致为：在字符串string中查找是否有符合正则表达式preg的子串，找到后将匹配结果存储在pmatch数组中，nmatch表示pmatch数组的大小，eflags用于控制匹配行为，默认为0
+				//返回值为0表示成功匹配，返回非0值表示没有匹配成功或发生错误
+				//这里的preg是&re[i]，string是e + position，nmatch是1，pmatch是&pmatch，eflags是0
+				//e + position表示从字符串e的position位置开始匹配
+				//pmatch是一个结构体，包含两个成员：rm_so和rm_eo，分别表示匹配子串在字符串中的起始和结束位置的偏移量
+				//pmatch.rm_so == 0 的意思是匹配的子串必须从当前位置开始
+				//综上所述，这个if条件的意思是：如果在字符串e从position位置开始，有符合规则re[i]的子串，并且这个子串从position位置开始
+				//如果if条件成立，说明在当前位置找到了一个符合规则的token
+
+				//printf("match rules[%d] = \"%s\" at position %d with len %d: %.*s\n", i, rules[i].regex, position, pmatch.rm_eo - pmatch.rm_so, pmatch.rm_eo - pmatch.rm_so, e + position);
+				//debug
+
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
 				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
+				//综上所述 这里实现了一个正确的token识别 我不想看了 看不懂
 
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array `tokens'. For certain types
@@ -98,6 +116,8 @@ static bool make_token(char *e) {
 				switch(rules[i].token_type) { //switch的格式：switch(令牌类型) { case: ...
 					//每种情况，向tokens数组添加类型名称 并记录这一子串。记录完成后，指针后移。
 					case NOTYPE: break; //忽略空格
+					case NUM:
+						
 
 					default: panic("please implement me");
 				}
