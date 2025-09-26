8d3a8f278d46484337397660762f548c0b0ff693
3024244169-Wang Haoyu <3160303297@qq.com>
2025-09-26 20:28:29 +0800

    备份

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -2,11 +2,12 @@
 
 #include "data-mov/mov.h"
 #include "data-mov/xchg.h"
+#include "data-mov/push.h"
+#include "data-mov/pop.h"
 #include "data-mov/movext.h"
 #include "data-mov/cltd.h"
-#include "data-mov/push.h"
 
-#include "arith/adc.h"
+#include "arith/cmp.h"
 #include "arith/dec.h"
 #include "arith/inc.h"
 #include "arith/neg.h"
@@ -14,11 +15,9 @@
 #include "arith/mul.h"
 #include "arith/idiv.h"
 #include "arith/div.h"
-#include "arith/sbb.h"
 #include "arith/sub.h"
-
-#include "control/jmp.h"
-#include "control/call.h"
+#include "arith/adc.h"
+#include "arith/sbb.h"
 
 #include "logic/and.h"
 #include "logic/or.h"
@@ -28,14 +27,18 @@
 #include "logic/shl.h"
 #include "logic/shr.h"
 #include "logic/shrd.h"
+#include "logic/test.h"
+
+#include "control/call.h"
+#include "control/ret.h"
+#include "control/jcc.h"
+#include "control/jmp.h"
 
 #include "string/rep.h"
+#include "string/movs.h"
 #include "string/scas.h"
 #include "string/stos.h"
-#include "string/movs.h"
 
 #include "misc/misc.h"
 
 #include "special/special.h"
-
-
